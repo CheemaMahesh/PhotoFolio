86b2e5a50ac1be0d043178aada6d5171fdf438b8
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-08-29 16:43:02 +0530

    PhotoFolio 1.1

--- src/Album.js
+++ src/Album.js
@@ -1,8 +1,11 @@
 import {useState,useEffect,useRef} from "react";
 import {db} from "./FirebaseInit";
+import { collection,doc,setDoc } from "firebase/firestore";
 
 export default function Album(){
-    const [AlbumN,setAlbum]=useState(null);
+    // const [AlbumN,setAlbum]=useState();
+    const [AlbumN, setAlbum] = useState(""); // Initialize with undefined
+
     const [Albums,setAlbums]=useState([]);
     const [btnD,setBtnd]=useState(false);
     const [formD,setFormd]=useState(false);
@@ -29,13 +32,24 @@ export default function Album(){
                 e.preventDefault();
                 setAlbum("");
             }
-            const handleAdd=(e)=>{
+            async function handleAdd(e) {
                 e.preventDefault();
-                setAlbums([...Albums, AlbumN]);
-                console.log(Albums);
-                setAlbum("");
+                try {
+                    const docRef = doc(collection(db, "photofolio"));
+            
+                  const mahi=  await setDoc(docRef, {
+                        title: AlbumN,
+                        createdOn: new Date()
+                    });
+                    console.log(mahi);
+            
+                    setAlbums([...Albums, AlbumN]);
+                    setAlbum(""); // Reset the input field
+                } catch (error) {
+                    console.error("Error adding album:", error);
+                }
             }
-
+            
     
     
     return(<div className="Album">
@@ -47,7 +61,7 @@ export default function Album(){
         <h1>Create an album</h1>
     </div>
     <div className="I-Body">
-        <input placeholder="Album Name" ref={titleRef} value={AlbumN} onChange={(e)=>setAlbum(e.target.value)} required/>
+<input placeholder="Album Name" ref={titleRef} value={AlbumN} onChange={(e)=>setAlbum(e.target.value)} required/>
         <button className="clear" onClick={handleClear}>Clear</button>
         <button className="create" onClick={handleAdd}>Create</button>
     </div>
@@ -62,6 +76,8 @@ export default function Album(){
         )}       </div>
                      
         </div>
-        <div className="Album-Display"></div>
+        <div className="Album-Display">{Albums.map((data,i)=>{
+            return(<h1 key={i}>{data}</h1>)
+        })}</div>
     </div>)
 }
